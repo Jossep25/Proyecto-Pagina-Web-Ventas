a69bd10c21ad2ef71fa93febb4e3a3a98a2c2a12
Jossep25 <lolitoscomor@gmail.com>
2024-05-23 18:49:23 -0500

    Actualizacion de informacion de user

--- model/users.kt
+++ model/users.kt
@@ -1,2 +1,10 @@
 package model
-data class users(val id: Int, val username: String, val password: String, val roleId: Int)
+data class users(val id: Int, 
+                val username: String, 
+                val password: String, 
+                val roleId: Int, 
+                val firstName: String, 
+                val lastName: String, 
+                val dni: String
+                //agregaremos mas
+                )
